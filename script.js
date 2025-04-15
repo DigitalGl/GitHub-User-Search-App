9b3d50787659fa7f2117a3d39988bae2fa62a07c
Daniil <daniil424342@gmail.com>
2025-04-15 15:21:23 +0500

    new commit

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const input = document.querySelector("[data-search-input]");
 const userInfoContainer = document.querySelector("[data-user-info-container]");
 const reposContainer = document.querySelector("[data-repos-container]");
 
+const API_GITHUB = "https://api.github.com/users";
+
 
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -15,9 +17,21 @@ form.addEventListener("submit", async (e) => {
     }
 
     try {
-        const userResponse = await fetch()
+        const userResponse = await fetch(`${API_GITHUB}/${username}`);
+        if (!userResponse.ok) throw new Error("User not Found");
+
+        const userData = await userResponse.json();
+
+        userInfoContainer.innerHTML = `
+        <div>
+            <img src="${userData.avatar_url}" alt="${userData.login}"/>
+            <h2>${userData.name || userData.login}</h2>
+            <p>${userData.bio || "No bio available"}</p>
+        </div>
+        `;
+
     } catch (error) {
-        
+        userInfoContainer.innerHTML = `<p>${error.message}</p>`
     }
 })
 
